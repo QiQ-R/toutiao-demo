bc481b7db150879e30ae297f5634b52f414d8461
QiQ-R <1713759180@qq.com>
2022-11-06 11:44:59 +0800

    添加字体图标

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
-
+import "./styles/icon.less"
 Vue.config.productionTip = false
 
 new Vue({
