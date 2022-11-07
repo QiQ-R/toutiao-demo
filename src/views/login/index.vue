9558d81a5a1c06446cdb6119bf356f131678e385
QiQ-R <1713759180@qq.com>
2022-11-07 11:14:28 +0800

    验证码校验手机号 和验证码倒计时

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,21 +1,39 @@
 <template>
   <div>
     <van-nav-bar title="登录" class="login" />
-    <van-form class="login-container" @submit="loginSub">
-      <van-field name="用户名" placeholder="请输入手机号" v-model="user.mobile">
+    <van-form class="login-container" @submit="loginSub" ref="sendcode">
+      <van-field
+        name="mobile"
+        placeholder="请输入手机号"
+        v-model="user.mobile"
+        type="number"
+        :rules="formRules.mobile"
+      >
         <i slot="left-icon" class="iconfont icon-shouji"></i>
       </van-field>
-      <van-field name="验证码" placeholder="请输入验证码" v-model="user.code">
+      <van-field
+        name="code"
+        placeholder="请输入验证码"
+        v-model="user.code"
+        :rules="formRules.code"
+      >
         <i slot="left-icon" class="iconfont icon-yanzhengma"></i>
         <template #button>
-          <van-button size="small" type="primary" class="send-sms-btn" round
-            >发送验证码</van-button
-          >
+          <van-button
+            size="small"
+            type="primary"
+            class="send-sms-btn"
+            round
+            native-type="button"
+            @click="sendBtn"
+            v-if="!flag">
+            发送验证码
+            </van-button>
+            <van-count-down :time="time" format="ss" v-else  @finish="flag=false"/>
         </template>
       </van-field>
       <div style="margin: 16px">
-        <van-button block type="info" native-type="submit" class="login-btn"
-          >提交</van-button
+        <van-button block type="info" native-type="submit" class="login-btn">提交</van-button
         >
       </div>
     </van-form>
@@ -31,16 +49,66 @@ export default {
         code: "",
         mobile: "",
       },
+      formRules: {
+        mobile: [
+          {
+            required: true,
+            message: "请填写手机号",
+          },
+          {
+            pattern: /^1\d{10}$/,
+            message: "请输入11位手机号",
+          },
+        ],
+        code: [
+          {
+            required: true,
+            message: "请填写验证码",
+          },
+          {
+            pattern: /^\d{6}$/,
+            message: "请输入6位验证码",
+          },
+        ],
+      },
+      time:5000,
+      flag:false
     };
   },
   methods: {
     async loginSub() {
-     try{
-       const res = await loginApi(this.user);
-      console.log("登录成功", res);
-     }catch(err){
-      console.log('登录失败');
-     }
+      this.$toast.loading({
+        message: "加载中...",
+        forbidClick: true,
+        duration: 0,
+      });
+      try {
+        const res = await loginApi(this.user);
+        this.$toast.success({
+          message: "登录成功",
+
+          duration: 2000,
+        });
+      } catch (err) {
+        this.$toast.fail({
+          message: "登录失败",
+          duration: 2000,
+        });
+      }
+    },
+    async sendBtn() {
+      // 验证手机号是否正确
+      try {
+        await this.$refs.sendcode.validate("mobile");
+        this.flag=true // 验证码倒计时
+      } catch (err) {
+        this.$toast.fail({
+          message: "验证失败",
+          duration: 2000,
+        });
+      }
+      
+     
     },
   },
 };
