90022ce3e67946603af5e969077af016ef87cd4a
QiQ-R <1713759180@qq.com>
2022-11-09 11:33:53 +0800

    首页导航栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,41 @@
 <template>
-  <div>
-    home页面
+  <div class="home-container">
+    <!-- 导航条 -->
+    <van-nav-bar title="标题">
+      <template #title>
+        <van-button
+          class="search-btn"
+          type="info"
+          size="small"
+          round
+          icon="search"
+          >搜索</van-button
+        >
+      </template>
+    </van-nav-bar>
+
+    <!--  -->
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped lang="less">
+.home-container {
+  /deep/.van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    .van-icon {
+      font-size: 32px;
+    }
+  }
+}
 </style>
